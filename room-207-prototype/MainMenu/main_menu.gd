a382916365f8c9901d1e9f566a38a89f79c07836
x40arcade <sobrepenakenneth97@gmail.com>
2026-06-05 18:27:55 +0800

    1.6.3.2
    Main menu

--- room-207-prototype/MainMenu/main_menu.gd
+++ room-207-prototype/MainMenu/main_menu.gd
@@ -1,5 +1,13 @@
 extends Control
 
+var FADE_IN_FINISHED : bool = false
+
+func _ready() -> void:
+	if not FADE_IN_FINISHED:
+		$ColorRect.visible = true
+		$AnimationPlayer.play("FadeIn")
+	
+
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://MainMenu/options.tscn")
 
@@ -14,3 +22,9 @@ func _on_exit_pressed() -> void:
 
 func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Ep1/Scenes/SceneA1.tscn")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "FadeIn":
+		$ColorRect.visible = false
+		FADE_IN_FINISHED = true
